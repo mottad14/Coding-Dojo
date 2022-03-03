03eeb74b1569b1de795bd64f57f2b90e4d5dbe02
Daniel Motta <mottad14@gmail.com>
2022-03-02 22:28:39 -0600

    Button function,  web design used HTML, JS, CSS

---
+++ LikeButton/script.js
@@ -0,0 +1,14 @@
+function likeButton1(){
+    var countNum = document.querySelector("#firstBox");
+    countNum.innerText= (parseInt(countNum.innerText)+1)
+}
+
+function likeButton2(){
+    var countNum = document.querySelector("#secondBox");
+    countNum.innerText= (parseInt(countNum.innerText)+1)
+}
+
+function likeButton3(){
+    var countNum = document.querySelector("#thirdBox");
+    countNum.innerText= (parseInt(countNum.innerText)+1)
+}
